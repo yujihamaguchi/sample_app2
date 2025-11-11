a0b053085091aecb370ad4512c2b4c6bdb4bc4d2
Yuji Hamaguchi <hamaguchi.u2@gmail.com>
2025-11-11 12:59:38 +0900

    WIP

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,49 @@
-require "test_helper"
+require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: 'Example User', email: 'user@example.com')
+  end
+
+  test 'should be valid' do
+    assert @user.valid?
+  end
+
+  test 'name should be present' do
+    @user.name = ' '
+    refute @user.valid?
+  end
+
+  test 'email should be present' do
+    @user.email = ' '
+    refute @user.valid?
+  end
+
+  test 'name should not be too long' do
+    @user.name = 'a' * 51
+    refute @user.valid?
+  end
+
+  test 'email should not be too long' do
+    @user.email = "#{'a' * 244}@example.com"
+    refute @user.valid?
+  end
+
+  test 'email validation should accept valid addresses' do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test 'email validation should reject invalid addresses' do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      refute @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
 end
