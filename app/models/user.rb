d25bea7a3572b0d80d346fe5de5fb4d828398642
Yuji Hamaguchi <hamaguchi.u2@gmail.com>
2025-11-13 13:06:53 +0900

    WIP

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
             length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: true
+  validates :password, presence: true, length: { minimum: 8 }
+  has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: 'Example User', email: 'user@example.com')
+    @user = User.new(name: 'Example User', email: 'user@example.com', password: 'foobarbaz',
+                     password_confirmation: 'foobarbaz')
   end
 
   test 'should be valid' do
@@ -58,5 +59,15 @@ class UserTest < ActiveSupport::TestCase
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test 'password should be present (nonblank)' do
+    @user.password = @user.password_confirmation = ' ' * 8
+    refute @user.valid?
+  end
+
+  test 'password should have a minimum length' do
+    @user.password = @user.password_confirmation = 'a' * 7
+    refute @user.valid?
   end
 end
