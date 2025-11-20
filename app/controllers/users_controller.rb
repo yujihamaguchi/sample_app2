a699964d1915033bae05985f4f8acce4a286f7f2
Yuji Hamaguchi <hamaguchi.u2@gmail.com>
2025-11-20 15:41:58 +0900

    WIP

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,24 @@
 class UsersController < ApplicationController
   def new
-    debugger
+    @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      puts ''
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params[:user].require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
