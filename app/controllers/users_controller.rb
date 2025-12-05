7555e7e5773202a2e6495dd02ea4daffda46f73d
Yuji Hamaguchi <hamaguchi.u2@gmail.com>
2025-12-05 11:34:34 +0900

    WIP

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = 'Profile updated'
+      redirect_to user_path(@user)
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
   private
 
   def user_params
---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+  test 'unsuccessful edit' do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: { name: '',
+                                              email: 'foo@invalid',
+                                              password: 'foo',
+                                              password_confirmation: 'bar' } }
+    assert_template 'users/edit'
+    assert_select 'div.alert', 'The form contains 4 errors.'
+  end
+
+  test 'successful edit' do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = 'Foo Bar'
+    email = 'foo@bar.com'
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              password: '',
+                                              password_confirmation: '' } }
+    refute flash.empty?
+    assert_redirected_to user_path(@user)
+    @user.reload
+    assert name, @user.name
+    assert email, @user.email
+  end
+end
