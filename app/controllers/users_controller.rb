1d2e0286c512c89f168e47d881149488b846193c
Yuji Hamaguchi <hamaguchi.u2@gmail.com>
2025-12-11 11:57:50 +0900

    WIP

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: %i[edit update index]
+  before_action :logged_in_user, only: %i[edit update index destroy]
   before_action :correct_user, only: %i[edit update]
+  before_action :admin_user, only: :destroy
   def new
     @user = User.new
   end
@@ -36,6 +37,12 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page])
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = 'User deleted'
+    redirect_to users_url, status: :see_other
+  end
+
   private
 
   def user_params
@@ -53,5 +60,9 @@ class UsersController < ApplicationController
   def correct_user
     @user = User.find(params[:id])
     redirect_to root_url, status: :see_other unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to root_url, status: :see_other unless current_user.admin?
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -42,4 +42,12 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get users_path
     assert_redirected_to login_url
   end
+
+  test 'should not allow the admin attribute to be edited via the web' do
+    log_in_as(@other_user)
+    refute @other_user.admin?
+    patch user_path(@other_user),
+          params: { user: { admin: true } }
+    refute @other_user.reload.admin?
+  end
 end
