e1619f404fd3a229d00adfd5f031195c387af1fc
Yuji Hamaguchi <hamaguchi.u2@gmail.com>
2026-01-09 17:20:14 +0900

    tweak

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UsersSignupTest < ActionDispatch::IntegrationTest
   test 'invalid signup information' do
     get signup_path
-    assert_difference 'User.count', 0 do
+    assert_no_difference 'User.count' do
       post users_path,
            params: { user: { name: '',
                              email: 'user@invalid',
